0b4641a8096590bde983288c0938e7d82bfd4805
Lucian Corlaciu <lucian@cliqz.com>
2017-06-12 23:10:09 +0200

    Mozilla partner repack telemetry

--- specific/firefox/cliqz@cliqz.com/bootstrap.js
+++ specific/firefox/cliqz@cliqz.com/bootstrap.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var CLIQZ;
+var CLIQZ,
+    telemetryReported = false;
 
 function startup(aData, aReason) {
   var global = {};
@@ -8,6 +9,44 @@ function startup(aData, aReason) {
   Services.scriptloader.loadSubScript('chrome://cliqz/content/CLIQZ.js', global);
   CLIQZ = global.CLIQZ;
   CLIQZ.start(aData, aReason);
+
+  // Give some time to the telemetry controller to initialize at startup
+  Components.utils.import('resource://gre/modules/Timer.jsm');
+  setTimeout(reportTelemetry, 60000);
+
+  // force sending a telemetry signal in case the shutdown
+  // happens before the 60 seconds timeout
+  Services.obs.addObserver(reportTelemetry, "profile-before-change", false);
+}
+
+function reportTelemetry(){
+  if(telemetryReported) return;
+
+  Components.utils.import('resource://gre/modules/TelemetryEnvironment.jsm');
+  Components.utils.import('resource://gre/modules/TelemetryController.jsm');
+  Components.utils.import('resource://gre/modules/Preferences.jsm');
+
+  const ping = TelemetryController.getCurrentPingData();
+
+  if (TelemetryEnvironment.setExperimentActive) { // FF53+
+    TelemetryEnvironment.setExperimentActive('cliqz-repack',
+      Services.appinfo.distributionID);
+  }
+
+  TelemetryController.submitExternalPing('cliqz-repack',
+    {
+      cliqzSession: Preferences.get('extensions.cliqz.session', null),
+      sessionId: ping.payload.info.sessionId,
+      subsessionId: ping.payload.info.subsessionId,
+      distributionId: Services.appinfo.distributionID
+    },
+    {
+      addClientId: true,
+      addEnvironment: true,
+    }
+  );
+
+  telemetryReported = true;
 }
 
 function shutdown(aData, aReason) {
